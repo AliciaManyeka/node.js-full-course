fe2866bf82387f872a2b19eaffb047f8983fb4f2
AliciaManyeka <amanyeka8@gmail.com>
2024-08-22 11:43:11 +0200

    third

--- index.js
+++ index.js
@@ -6,9 +6,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    
-  res.write("Welcome to our home page");
-  res.end();
-
+  //    property tht gets the url //
+  if (req.url === "/") {
+    res.end("Welcome to our home page");
+  }
+  if (req.url === "./about") {
+    res.end("here is our short history");
+  }
+  res.end(
+    <h1>Opps!</h1>,
+    <p>We cant to seem to find the page you are looking for</p>
+  );
 });
 server.listen(5000);
